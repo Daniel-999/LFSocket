d5e64b8e6fe68d8fce4f44605e341be287e88975
wangxiaoxiang <wangxiaoxiang-cn@foxmail.com>
2016-08-16 20:07:20 +0800

    init

---
+++ LFSocketIOKit/LFSocketIOKit/LFSocketIOKit.h
@@ -0,0 +1,25 @@
+//
+//  LFSocketIOKit.h
+//  LFSocketIOKit
+//
+//  Created by wangxiaoxiang on 16/8/16.
+//  Copyright © 2016年 wangxiaoxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LFSocketIOKit.
+FOUNDATION_EXPORT double LFSocketIOKitVersionNumber;
+
+//! Project version string for LFSocketIOKit.
+FOUNDATION_EXPORT const unsigned char LFSocketIOKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LFSocketIOKit/PublicHeader.h>
+
+#import <LFSocketIOKit/SocketIO.h>
+#import <LFSocketIOKit/SocketIOJSONSerialization.h>
+#import <LFSocketIOKit/SocketIOPacket.h>
+#import <LFSocketIOKit/SocketIOTransport.h>
+#import <LFSocketIOKit/SocketIOTransportWebsocket.h>
+#import <LFSocketIOKit/SocketIOTransportXHR.h>
+
---
+++ SRWebSocketKit/SRWebSocketKit/SRWebSocketKit.h
@@ -0,0 +1,20 @@
+//
+//  SRWebSocketKit.h
+//  SRWebSocketKit
+//
+//  Created by wangxiaoxiang on 16/8/16.
+//  Copyright © 2016年 wangxiaoxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SRWebSocketKit.
+FOUNDATION_EXPORT double SRWebSocketKitVersionNumber;
+
+//! Project version string for SRWebSocketKit.
+FOUNDATION_EXPORT const unsigned char SRWebSocketKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SRWebSocketKit/PublicHeader.h>
+#import <SRWebSocketKit/SRWebSocket.h>
+
+
